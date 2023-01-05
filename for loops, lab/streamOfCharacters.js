6aa45f9c13e19930bc8b5f77a6492876736ad870
Petar Cholakov <petar.cholakov30@gmail.com>
2023-01-05 13:26:58 +0000

    for loops, exercise

---
+++ for loops, lab/streamOfCharacters.js
@@ -0,0 +1,12 @@
+//Write a function which receives text (string) and prints out every character of the string on a separate line.
+
+function streamOfCharacters(input) {
+    let text = input[0];
+
+    for (let i = 0; i <= text.length -1; i++) {
+        console.log(text[i]);
+    }
+}
+
+// streamOfCharacters(["softuni"]);
+// streamOfCharacters(["ice cream"]);
