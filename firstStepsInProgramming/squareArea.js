588b08ce874b2c93602bc62837a043bb95a737d9
Petar Cholakov <petar.cholakov30@gmail.com>
2023-01-08 20:53:00 +0000

    exercise

---
+++ firstStepsInProgramming/squareArea.js
@@ -0,0 +1,10 @@
+//Find the square area of a cube
+
+function squareArea(input) {
+    let length = Number(input[0]);
+    let area = length * length;
+
+    console.log(area);
+}
+
+squareArea(["5"])
