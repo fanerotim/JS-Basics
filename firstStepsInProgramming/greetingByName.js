c4cb2290ac538e4511873e8a9db116e241d44f87
Petar Cholakov <petar.cholakov30@gmail.com>
2023-01-08 21:00:35 +0000

    exercise

---
+++ firstStepsInProgramming/greetingByName.js
@@ -0,0 +1,9 @@
+//Write a function that greets the user by name:
+
+function greetingByName(input) {
+    let name = input[0];
+
+    console.log(`Hello, ${name}!`);
+}
+
+greetingByName(["Petar"]);
