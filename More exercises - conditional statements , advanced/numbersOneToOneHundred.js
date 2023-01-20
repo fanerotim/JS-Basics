72332ba3b71aed11d045105131544b3f87a62aad
Petar Cholakov <petar.cholakov30@gmail.com>
2023-01-20 09:19:27 +0000

    exercise

---
+++ More exercises - conditional statements , advanced/numbersOneToOneHundred.js
@@ -0,0 +1,13 @@
+//Write a function that prints out the numbers from 1 to 10.
+//Use while loop
+
+function printNumbers() {
+    let currentNum = 0;
+    
+    while(currentNum < 10) {
+        currentNum = currentNum + 1;
+        console.log(currentNum)
+    }
+}
+
+printNumbers(["10"])
