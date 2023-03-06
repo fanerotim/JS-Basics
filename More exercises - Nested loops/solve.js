9f75d8800e46b2d87833ecb9c99760349517f602
Petar Cholakov <petar.cholakov30@gmail.com>
2023-03-06 21:21:36 +0000

    Create solve.js

---
+++ More exercises - Nested loops/solve.js
@@ -0,0 +1,73 @@
+function uniquePinCodes(input) {
+    let firstNum = Number(input[0]);
+    let secondNum = Number(input[1]);
+    let thirdNum = Number(input[2]);
+
+    let intervalStart = 2;
+    let intervalEnd = 7;
+
+    let numOne = 0;
+    let numTwo = 0;
+    let numThree = 0;
+
+    for (let firstDigit = 1; firstDigit <= firstNum; firstDigit++) {
+        if (firstDigit % 2 === 0) {
+            numOne = firstDigit;
+        } else {
+            continue;
+        }
+
+        for (let secondDigit = 2; secondDigit <= secondNum; secondDigit++) {
+            // secondDigit starts from 2 as we are looking for all prime numbres
+            // since non-prime numbers can also be devided by 1, we start from 2
+            let counter = 0;
+            let isEnd = false;
+
+            if (secondDigit > intervalEnd) {
+                continue;
+            }
+
+                //i represents the numbers in the interval (2-7)
+                for (let i = intervalStart; i <= intervalEnd; i++) {
+                    if (secondDigit % i === 0) {
+                        counter++;
+                    }
+
+                    if (i === intervalEnd) {
+                        if (counter === 1) {
+                            numTwo = secondDigit;  
+                        } else {
+                            isEnd = true;
+                            continue;
+                        }
+                    }
+                }
+
+                if (isEnd === true) {
+                    continue;
+                }
+                
+            for (let thirdDigit = 1; thirdDigit <= thirdNum; thirdDigit++) {
+                if (thirdDigit % 2 === 0) {
+                    numThree = thirdDigit; 
+                    
+            } else {
+                continue;
+            }
+            console.log(`${numOne} ${numTwo} ${numThree}`)
+        }  
+    }
+}   
+}
+
+uniquePinCodes([
+    "3",
+    "100",
+    "5"
+    ])
+
+// uniquePinCodes([
+//     "8",
+//     "2",
+//     "8"
+//     ])
